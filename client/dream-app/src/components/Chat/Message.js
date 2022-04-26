d3ac331622087f4349ba023fd868aed374afe717
Xinlei Bian <bian.xin@northeastern.edu>
2022-04-26 03:01:20 -0400

    Add File Componenet

--- client/dream-app/src/components/Chat/Message.js
+++ client/dream-app/src/components/Chat/Message.js
@@ -4,16 +4,17 @@ import {
   ListItem,
   ListItemText,
   ListItemAvatar,
-  Box,
   Card,
   CardMedia,
   CardContent,
   Typography,
+  CardActions,
+  Button,
 } from "@mui/material";
 import ThemeOptions from "../../Theme";
 import noPhoto from "../../images/no_photo.jpg";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
+import fileIcon from "../../images/file.jpg";
+import FileSaver from "file-saver";
 
 //The Newest Message from chat friend(s) with photo
 export const MessageLeftNewest = (props) => {
@@ -173,7 +174,7 @@ export const ImageRightNewest = (props) => {
   );
 };
 
-//The Newest Image from chat friend(s) with photo
+//The Image from chat friend(s) with photo
 export const ImageLeft = (props) => {
   const timestamp = props.timestamp ? props.timestamp : "xx:xx";
   const image = props.image ? props.image : noPhoto;
@@ -197,7 +198,7 @@ export const ImageLeft = (props) => {
   );
 };
 
-//The newest Image from userself with photo
+//The Image from userself with photo
 export const ImageRight = (props) => {
   const timestamp = props.timestamp ? props.timestamp : "xx:xx";
   const image = props.image ? props.image : noPhoto;
@@ -216,6 +217,165 @@ export const ImageRight = (props) => {
           component="img"
           image={image}
         />
+      </Card>
+    </ListItem>
+  );
+};
+const downloadFile = (data, fileName) => {
+  FileSaver.saveAs(data, fileName);
+};
+
+//The Newest File from chat friend(s)
+export const FileLeftNewest = (props) => {
+  const timestamp = props.timestamp ? props.timestamp : "xx:xx";
+  const photoURL = props.photoURL ? props.photoURL : "";
+  const fileBlob = props.file ? props.file : "";
+  const fileName = props.fileName ? props.fileName : "";
+  return (
+    <ListItem sx={{ pt: 0, pb: 0, width: "50%" }}>
+      <ListItemAvatar>
+        <Avatar variant="square" src={photoURL} />
+      </ListItemAvatar>
+      <Card style={{ maxWidth: 200, margin: 15 }}>
+        <CardMedia
+          style={{
+            width: "auto",
+            maxHeight: "200px",
+          }}
+          component="img"
+          image={fileIcon}
+        />
+        <CardContent>
+          <Typography variant="body2">{fileName}</Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            size="small"
+            onClick={() => {
+              downloadFile(fileBlob, fileName);
+            }}
+          >
+            Download
+          </Button>
+        </CardActions>
+      </Card>
+      <Typography variant="body2" color="text.secondary">
+        {timestamp}
+      </Typography>
+    </ListItem>
+  );
+};
+
+//The File from chat friend(s)
+export const FileRightNewest = (props) => {
+  const timestamp = props.timestamp ? props.timestamp : "xx:xx";
+  const photoURL = props.photoURL ? props.photoURL : "";
+  const fileBlob = props.file ? props.file : "";
+  const fileName = props.fileName ? props.fileName : "";
+  return (
+    <ListItem sx={{ pt: 0, pb: 0, width: "50%", ml: 42 }}>
+      <Typography variant="body2" color="text.secondary">
+        {timestamp}
+      </Typography>
+      <Card style={{ maxWidth: 200, margin: 15 }}>
+        <CardMedia
+          style={{
+            width: "auto",
+            maxHeight: "200px",
+          }}
+          component="img"
+          image={fileIcon}
+        />
+        <CardContent>
+          <Typography variant="body2">{fileName}</Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            size="small"
+            onClick={() => {
+              downloadFile(fileBlob, fileName);
+            }}
+          >
+            Download
+          </Button>
+        </CardActions>
+      </Card>
+      <ListItemAvatar sx={{ display: "flex", justifyContent: "flex-end" }}>
+        <Avatar variant="square" src={photoURL}></Avatar>
+      </ListItemAvatar>
+    </ListItem>
+  );
+};
+
+//The File from chat friend(s)
+export const FileLeft = (props) => {
+  const timestamp = props.timestamp ? props.timestamp : "xx:xx";
+  const fileBlob = props.file ? props.file : "";
+  const fileName = props.fileName ? props.fileName : "";
+  return (
+    <ListItem sx={{ pt: 0, pb: 0, width: "50%" }}>
+      <Card style={{ maxWidth: 200, margin: 15 }}>
+        <CardMedia
+          style={{
+            width: "auto",
+            maxHeight: "200px",
+          }}
+          component="img"
+          image={fileIcon}
+        />
+        <CardContent>
+          <Typography variant="body2">{fileName}</Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            size="small"
+            onClick={() => {
+              downloadFile(fileBlob, fileName);
+            }}
+          >
+            Download
+          </Button>
+        </CardActions>
+      </Card>
+      <Typography variant="body2" color="text.secondary">
+        {timestamp}
+      </Typography>
+    </ListItem>
+  );
+};
+
+//The File from chat friend(s)
+export const FileRight = (props) => {
+  const timestamp = props.timestamp ? props.timestamp : "xx:xx";
+  const fileBlob = props.file ? props.file : "";
+  const fileName = props.fileName ? props.fileName : "";
+  return (
+    <ListItem sx={{ pt: 0, pb: 0, width: "50%", ml: 42 }}>
+      <Typography variant="body2" color="text.secondary">
+        {timestamp}
+      </Typography>
+      <Card style={{ maxWidth: 200, margin: 15 }}>
+        <CardMedia
+          style={{
+            width: "auto",
+            maxHeight: "200px",
+          }}
+          component="img"
+          image={fileIcon}
+        />
+        <CardContent>
+          <Typography variant="body2">{fileName}</Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            size="small"
+            onClick={() => {
+              downloadFile(fileBlob, fileName);
+            }}
+          >
+            Download
+          </Button>
+        </CardActions>
       </Card>
     </ListItem>
   );
